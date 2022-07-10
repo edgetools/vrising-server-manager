9f1228457ca3d17b5ed6e25295b5c6d159c3efea
edgetools <53495555+edgetools@users.noreply.github.com>
2022-07-09 22:43:31 -0500

    set default param set

--- .etc/Publish-SemanticRelease.ps1
+++ .etc/Publish-SemanticRelease.ps1
@@ -7,6 +7,7 @@
 # https://www.conventionalcommits.org/en/v1.0.0/#summary
 
 param(
+    [CmdletBinding(DefaultParameterSetName='Publish')]
     [Parameter(ParameterSetName='Publish')]
     [switch]$Publish,
     [Parameter(ParameterSetName='Publish')]
