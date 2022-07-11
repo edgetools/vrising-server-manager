7fa2ef2202ed3dccbf485d2e2863a10f295ae6f8
edgetools <53495555+edgetools@users.noreply.github.com>
2022-07-11 02:47:06 -0500

    feat: check for new versions on startup
    supports custom behavior on import using VRisingServerManagerFlags

--- Source/Module/footer.ps1
+++ Source/Module/footer.ps1
@@ -10,3 +10,27 @@ $Global:PSDefaultParameterValues['Restart-VRisingServer:ErrorAction'] = 'Continu
 
 # custom formatters
 Update-FormatData -AppendPath "$PSScriptRoot\VRisingServerManager.Format.ps1xml"
+
+# check for new version
+if ('SkipNewVersionCheck' -notin $VRisingServerManagerFlags) {
+    $private:latestModule = Find-Module `
+        -Name 'VRisingServerManager' `
+        -Repository PSGallery `
+        -MinimumVersion $ModuleVersion
+    if ($ModuleVersion -ne $private:latestModule.Version) {
+        $releaseNotesWarning = ($private:latestModule.ReleaseNotes.Split(@("`r`n", "`r", "`n"), [System.StringSplitOptions]::None) |
+            ForEach-Object { "WARNING: $_" }) -join ([System.Environment]::NewLine)
+        Write-Warning "-- New Version Available! --"
+        Write-Warning ''
+        Write-Warning "Current Version: $ModuleVersion"
+        Write-Warning "Latest Version: $($private:latestModule.Version)"
+        Write-Warning ''
+        Write-Warning "To update, run:"
+        Write-Warning "  Update-Module -Name VRisingServerManager"
+        Write-Warning ''
+        Write-Warning "To disable this check:"
+        Write-Warning "  `$VRisingServerManagerFlags += ('SkipNewVersionCheck')"
+        Write-Warning ''
+        Write-Warning "-- Release Notes --$([System.Environment]::NewLine)$($releaseNotesWarning)"
+    }
+}
--- Source/Module/header.ps1
+++ Source/Module/header.ps1
@@ -1,2 +1,9 @@
 # module parameters
 $ErrorActionPreference = 'Stop'
+
+$script:ModuleVersion = (Import-PowerShellDataFile -Path (Join-Path -Path $PSScriptRoot -ChildPath 'VRisingServerManager.psd1')).ModuleVersion
+Write-Host "VRisingServerManager v$ModuleVersion"
+
+if ($false -eq [string]::IsNullOrEmpty($VRisingServerManagerFlags)){
+    Write-Host "Using VRisingServerManagerFlags: $VRisingServerManagerFlags"
+}
